4c28389819ad0581801d5ff993ee471d2035e2e3
Erkosh04 <erkoja1104@gmail.com>
2025-11-24 08:48:52 +0500

    Refactor train_lda to use more passes in LDA model
    Updated LDA model training function to include more passes for better topic coherence.

--- lda_model.py
+++ lda_model.py
@@ -1,25 +1,16 @@
 from gensim import corpora, models
-from gensim.models.coherencemodel import CoherenceModel
 
 def train_lda(texts):
-    tokenized = [t.split() for t in texts]
+    tokenized = [text.split() for text in texts]
     dictionary = corpora.Dictionary(tokenized)
-    corpus = [dictionary.doc2bow(text) for text in tokenized]
+    corpus = [dictionary.doc2bow(t) for t in tokenized]
 
-    lda = models.LdaModel(
+    lda_model = models.LdaModel(
         corpus=corpus,
         id2word=dictionary,
         num_topics=3,
-        random_state=42
+        passes=10
     )
 
-    topics = lda.print_topics()
-
-    coherence = CoherenceModel(
-        model=lda,
-        texts=tokenized,
-        dictionary=dictionary,
-        coherence='c_v'
-    ).get_coherence()
-
-    return topics, coherence
+    topics = lda_model.print_topics()
+    return topics
