745b689d0adb7374c85e5d31175db016711587ce
Erkosh04 <erkoja1104@gmail.com>
2025-11-24 08:49:33 +0500

    Refactor BERTopic training with dummy data
    Added dummy training data and modified training process.

--- bertopic_model.py
+++ bertopic_model.py
@@ -1,6 +1,17 @@
 from bertopic import BERTopic
 
-def train_bertopic(texts):
-    model = BERTopic()
-    topics, _ = model.fit_transform(texts)
-    return model.get_topic_info()
+# Dummy training data (BERTopic requires multiple docs)
+dummy_texts = [
+    "technology is advancing rapidly in modern society",
+    "artificial intelligence impacts human life",
+    "internet and smartphones changed communication",
+    "science and innovation drive progress",
+    "education improves with digital tools"
+]
+
+model = BERTopic()
+model.fit(dummy_texts)
+
+def get_bert_topic(text):
+    topics, _ = model.transform([text])
+    return topics
