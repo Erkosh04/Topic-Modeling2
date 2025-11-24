28e6b7a9596732b537db7b16d0d66e046331d2d9
Erkosh04 <erkoja1104@gmail.com>
2025-11-24 09:05:44 +0500

    Enhance train_lda with coherence measurement
    Added coherence calculation for LDA model.

--- lda_model.py
+++ lda_model.py
@@ -1,15 +1,23 @@
 from gensim import corpora, models
+from gensim.models.coherencemodel import CoherenceModel
 
 def train_lda(texts):
     tokenized = [t.split() for t in texts]
     dictionary = corpora.Dictionary(tokenized)
-    corpus = [dictionary.doc2bow(t) for t in tokenized]
+    corpus = [dictionary.doc2bow(text) for text in tokenized]
 
-    lda = models.LdaModel(
-        corpus=corpus,
-        id2word=dictionary,
-        num_topics=3,
-        passes=10
-    )
+    lda = models.LdaModel(corpus=corpus,
+                          id2word=dictionary,
+                          num_topics=3,
+                          random_state=42)
 
-    return lda.print_topics()
+    topics = lda.print_topics()
+
+    coherence = CoherenceModel(
+        model=lda,
+        texts=tokenized,
+        dictionary=dictionary,
+        coherence='c_v'
+    ).get_coherence()
+
+    return topics, coherence
