1eec90b0a31f54f73b4e7ee5c778ea4a0734f80b
Erkosh04 <erkoja1104@gmail.com>
2025-11-24 09:06:24 +0500

    Replace BERTopic training with LDA model training

--- lda_model.py
+++ lda_model.py
@@ -1,6 +1,23 @@
-from bertopic import BERTopic
+from gensim import corpora, models
+from gensim.models.coherencemodel import CoherenceModel
 
-def train_bertopic(texts):
-    model = BERTopic()
-    topics, _ = model.fit_transform(texts)
-    return model.get_topic_info()
+def train_lda(texts):
+    tokenized = [t.split() for t in texts]
+    dictionary = corpora.Dictionary(tokenized)
+    corpus = [dictionary.doc2bow(text) for text in tokenized]
+
+    lda = models.LdaModel(corpus=corpus,
+                          id2word=dictionary,
+                          num_topics=3,
+                          random_state=42)
+
+    topics = lda.print_topics()
+
+    coherence = CoherenceModel(
+        model=lda,
+        texts=tokenized,
+        dictionary=dictionary,
+        coherence='c_v'
+    ).get_coherence()
+
+    return topics, coherence
