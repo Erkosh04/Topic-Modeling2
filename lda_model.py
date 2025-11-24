263939f62135f2fe850cf01311e165683cf154b0
Erkosh04 <erkoja1104@gmail.com>
2025-11-24 09:00:05 +0500

    Refactor train_lda function for clarity

--- lda_model.py
+++ lda_model.py
@@ -1,16 +1,15 @@
 from gensim import corpora, models
 
 def train_lda(texts):
-    tokenized = [text.split() for text in texts]
+    tokenized = [t.split() for t in texts]
     dictionary = corpora.Dictionary(tokenized)
     corpus = [dictionary.doc2bow(t) for t in tokenized]
 
-    lda_model = models.LdaModel(
+    lda = models.LdaModel(
         corpus=corpus,
         id2word=dictionary,
         num_topics=3,
         passes=10
     )
 
-    topics = lda_model.print_topics()
-    return topics
+    return lda.print_topics()
