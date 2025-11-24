987024b356f0f5d188f4d053f7605c66ef05e7b6
Erkosh04 <erkoja1104@gmail.com>
2025-11-24 08:48:25 +0500

    Refactor text preprocessing with tokenization

--- preprocessing.py
+++ preprocessing.py
@@ -1,18 +1,18 @@
+import re
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
-import re
 
+nltk.download("punkt")
 nltk.download("stopwords")
 nltk.download("wordnet")
 
 stop_words = set(stopwords.words("english"))
-lemm = WordNetLemmatizer()
+lemmatizer = WordNetLemmatizer()
 
 def preprocess_text(text):
     text = text.lower()
-    text = re.sub(r"[^a-zA-Z ]", "", text)
-    words = text.split()
-    words = [w for w in words if w not in stop_words]
-    words = [lemm.lemmatize(w) for w in words]
-    return " ".join(words)
+    text = re.sub(r"[^a-z\s]", "", text)
+    tokens = nltk.word_tokenize(text)
+    tokens = [lemmatizer.lemmatize(word) for word in tokens if word not in stop_words]
+    return " ".join(tokens)
